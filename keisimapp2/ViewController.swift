5105807dd5bbc76a0740bfab66e84ef71a403f97
keisim <bokukeisuke@gmail.com>
2015-07-17 23:12:23 +0900

    Initial Commit

---
+++ keisimapp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  keisimapp2
+//
+//  Created by 東田圭介 on 2015/07/17.
+//  Copyright (c) 2015年 東田圭介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
