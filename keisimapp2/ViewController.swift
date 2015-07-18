05916d764ec1829beebcf82fb2f2029bd7cff71c
keisim <bokukeisuke@gmail.com>
2015-07-18 09:36:41 +0900

    first commit

--- keisimapp2/ViewController.swift
+++ keisimapp2/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.view.addGestureRecognizer(myTap) // 割り勘アプリで追記
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +21,47 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func tapbtn(sender: UIButton) {
+        var twitterVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        twitterVC.setInitialText("つぶやくよ")
+        presentViewController(twitterVC, animated: true, completion:nil)
+    }
+    
+    @IBAction func returnMenu(segue: UIStoryboardSegue){
+    }
 
-
+    // 割り勘計算アプリ　http://ilog.hateblo.jp/entry/2015/03/30/203044
+    
+    @IBOutlet weak var payment: UITextField!
+    @IBOutlet weak var persons: UITextField!
+    @IBOutlet weak var answer: UILabel!
+    
+    
+    @IBOutlet var myTap: UITapGestureRecognizer!
+    var message: String!
+    
+    @IBAction func tapScreen(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    // 1人あたりの金額の計算と結果表示
+    @IBAction func calcAverage(sender: UIButton) {
+        var sum = payment.text.toInt()
+        var number = persons.text.toInt()
+        
+        println(sum! / number!)
+        answer.text = "\((sum! / number!).description)円"
+        message = "\(sum! / number!)円を準備してください"
+    }
+    
+    //　リセット
+    @IBAction func resetValue(sender: UIButton) {
+        payment.text = ""
+        persons.text = ""
+        answer.text = ""
+        message = ""
+    }
+    
 }
 
---
+++ keisimapp2/nextViewController.swift
@@ -0,0 +1,21 @@
+//
+//  nextViewController.swift
+//  keisimapp2
+//
+//  Created by 東田圭介 on 2015/07/17.
+//  Copyright (c) 2015年 東田圭介. All rights reserved.
+//
+
+import UIKit
+
+class nextViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+    
+    
+}
